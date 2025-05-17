fda2182032e2a1ea8f921f5b83dd8ab32253adf7
killbyfire <112400333+Killbyfire@users.noreply.github.com>
2025-05-17 22:19:59 +0200

    Add complete logic for hangman to function

---
+++ public/js/hangman.js
@@ -0,0 +1,132 @@
+const hangmanContainer = document.querySelector(".hangmanContainer");
+const guessContainer = document.querySelector(".guessContainer");
+const correctGuessContainer = document.querySelector(".correctContainer");
+
+const guessesLeftText = document.querySelector(".guessesLeft");
+const alertDialog = document.querySelector(".guessInput .alertDialog");
+const guessInput = document.querySelector(".guessInput input");
+
+const resetButton = document.querySelector(".resetButton");
+const giveUpButton = document.querySelector(".giveUpButton");
+
+const maxGuesses = 6;
+let guessesLeft = maxGuesses;
+
+const wordListLanguage = "dutch";
+
+let pickedWord = "temp";
+let pickedWordArray = pickedWord.split("");
+
+let correctGuessedLetters = [];
+let currentGuessedLetters = [];
+
+resetButton.addEventListener("click", (ev) => {
+  resetGame();
+});
+
+giveUpButton.addEventListener("click", (ev) => {
+  alert(`The word was: ${pickedWord}`);
+  resetGame();
+});
+
+guessInput.addEventListener("keyup", (ev) => {
+  const currentLetter = ev.target.value.trim();
+
+  if (!currentLetter) {
+    return;
+  }
+
+  guessInput.value = "";
+
+  if (currentGuessedLetters.includes(currentLetter)) {
+    alertDialog.innerText = `Letter ${currentLetter} is al gegokt`;
+    return;
+  }
+
+  alertDialog.innerText = "";
+
+  let notInWord = true;
+  pickedWordArray.forEach((pickedWordLetter, index) => {
+    if (pickedWordLetter === currentLetter) {
+      notInWord = false;
+      correctGuessedLetters[index] = currentLetter;
+    }
+  });
+
+  if (notInWord) {
+    guessesLeft -= 1;
+  }
+
+  currentGuessedLetters.push(currentLetter);
+
+  UpdateGuessedLetters();
+});
+
+function generatePlaceholderNumbers() {
+  const pickedWordLength = pickedWord.length;
+  for (let i = 0; i < pickedWordLength; i++) {}
+}
+
+UpdateGuessedLetters();
+
+function UpdateGuessedLetters() {
+  guessContainer.innerHTML = "";
+  correctGuessContainer.innerHTML = "";
+  guessesLeftText.innerHTML = "";
+
+  if (guessesLeft <= 0) {
+    alert(`You lost! The word was: ${pickedWord}`);
+  }
+
+  if (correctGuessedLetters.join("") === pickedWord) {
+    alert(`You won! The word was: ${pickedWord}`);
+  }
+
+  guessesLeftText.innerText = `Guesses left: ${guessesLeft}`;
+
+  currentGuessedLetters.forEach((guessedLetter) => {
+    const letterSpan = document.createElement("span");
+
+    letterSpan.classList.add("guessedLetter");
+    letterSpan.innerText = guessedLetter;
+
+    guessContainer.appendChild(letterSpan);
+  });
+
+  pickedWord.split("").forEach((wordLetter, index) => {
+    const letterSpan = document.createElement("span");
+
+    letterSpan.classList.add("guessedLetter");
+
+    if (correctGuessedLetters[index] === wordLetter) {
+      letterSpan.innerText = wordLetter;
+    } else {
+      letterSpan.innerText = "_";
+    }
+
+    correctGuessContainer.appendChild(letterSpan);
+  });
+}
+
+async function pickNewWord() {
+  const wordList = await fetch(`/assets/${wordListLanguage}.txt`);
+
+  const text = await wordList.text();
+  const lines = text.split("\n");
+
+  return lines[Math.floor(Math.random() * lines.length)].trim();
+}
+
+async function resetGame() {
+  pickedWord = await pickNewWord();
+  pickedWordArray = pickedWord.split("");
+
+  correctGuessedLetters = [];
+  currentGuessedLetters = [];
+
+  guessesLeft = maxGuesses;
+
+  UpdateGuessedLetters();
+}
+
+resetGame();
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
 const { Server } = require("socket.io");
+const crypto = require("crypto");
+
+crypto.randomUUID();
 
 const app = express();
 const server = http.createServer(app);
@@ -15,6 +19,10 @@ const port = 3000;
 const rooms = {};
 
 app.use(express.static(path.join(__dirname, publicFolder)));
+
+io.on("connection", (socket) => {
+  
+});
 
 server.listen(port, () => {
   console.log("Listening on port", port);
